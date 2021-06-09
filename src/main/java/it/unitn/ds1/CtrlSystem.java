b20438fa122dfcb26f577b8b99c4c433445461b6
zeeros <1713464+zeeros@users.noreply.github.com>
2021-06-09 17:29:45 +0200

    basic server initialization with distributed datastore

--- src/main/java/it/unitn/ds1/CtrlSystem.java
+++ src/main/java/it/unitn/ds1/CtrlSystem.java
@@ -2,9 +2,12 @@ package it.unitn.ds1;
 import java.io.IOException;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import java.util.HashMap;
 
 public class CtrlSystem {
   final static int N_CLIENTS = 6;
+  final static int N_SERVERS = 3;
+  final static int MAX_KEY = 10;
 
   public static void main(String[] args) {
     // Create an actor system named "ctrlakka"
@@ -14,6 +17,17 @@ public class CtrlSystem {
     for (int i=0; i<N_CLIENTS; i++) {
       System.out.println("client"+i);
       system.actorOf(TxnClient.props(i), "client" + i);
+    }
+
+    // Create multiple Server actors
+    for (int i=0; i<N_SERVERS; i++) {
+      System.out.println("server"+i);
+      HashMap<Integer, Integer> datastore = new HashMap<Integer,Integer>();
+      for(int j=0; j<MAX_KEY; j++){
+	  Integer k = (i*MAX_KEY) + j;
+	  datastore.put(k, 10);
+      }
+      system.actorOf(Server.props(i, datastore), "server" + i);
     }
 
     System.out.println("Press ENTER to exit");
