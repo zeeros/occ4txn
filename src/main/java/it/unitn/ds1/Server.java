3467bf4f5142b548be15a32174906cc424e4bb0d
zeeros <1713464+zeeros@users.noreply.github.com>
2021-07-06 18:01:22 +0200

    identation

--- src/main/java/it/unitn/ds1/Server.java
+++ src/main/java/it/unitn/ds1/Server.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 public class Server extends AbstractActor {
 	private final Integer serverId;
 //list of every Private Workspaces in the server
-    static Map<Integer,PrivateWorkspace> privateWorkspaces = new HashMap<Integer,PrivateWorkspace>();
+	static Map<Integer, PrivateWorkspace> privateWorkspaces = new HashMap<Integer, PrivateWorkspace>();
 	private static final Logger log = LogManager.getLogger(Server.class);
 
 	// TXN operation (move some amount from a value to another)
@@ -30,11 +30,13 @@ public class Server extends AbstractActor {
 	static public Props props(int serverId, Map<Integer, DataItem> datastore) {
 		return Props.create(Server.class, () -> new Server(serverId, datastore));
 	}
+
 	/*-- Private workspace class ------------------------------------------------------ */
 	public static class PrivateWorkspace {
 		private Txn txn;
 		private HashMap<Integer, DataItem> writeCopies;
 		private HashMap<Integer, DataItem> readCopies;
+
 		public PrivateWorkspace(Txn txn) {
 			this.txn = txn;
 			this.writeCopies = new HashMap<Integer, DataItem>();
@@ -47,99 +49,103 @@ public class Server extends AbstractActor {
 	// WRITE request from the coordinator to the server
 	public static class WriteMsg implements Serializable {
 	}
-	
+
 	public static class TxnVoteMsg implements Serializable {
 		public Txn txn;
 		public Boolean vote;
 		public Integer serverId;
+
 		public TxnVoteMsg(Txn txn, Boolean vote, Integer serverId) {
 			this.txn = txn;
 			this.vote = vote;
 			this.serverId = serverId;
 		}
 	}
-	
-	
 
 	/*-- Message handlers ----------------------------------------------------- */
 
 	private void OnReadMsg(Coordinator.ReadMsg msg) {
 		Txn txn = msg.txn;
 		DataOperation dataoperation = msg.dataoperation;
-		//if no private workspace : creation process and write the new data value within
-				PrivateWorkspace pw = privateWorkspaces.get(txn.hashCode());
-				// Retrieve the current version
-				DataItem dataItemCopy = dataoperation.getDataItem();
-				log.debug("server" + serverId + "<--[READ(" + dataoperation.getKey() + ")]--coordinator" + txn.getCoordinatorId());
-				if (pw == null) {
-			    	pw = new PrivateWorkspace(txn);
-			    	privateWorkspaces.put(txn.hashCode(), pw);
-			    	//copy of the dataitem that will be temporary stored in the private workspace
-			    	
-					pw.readCopies.put(dataoperation.getKey(), dataItemCopy);
-			    }
-			    else  {
-			    	pw.readCopies.put(dataoperation.getKey(), dataItemCopy);
-			    }
+		// if no private workspace : creation process and write the new data value
+		// within
+		PrivateWorkspace pw = privateWorkspaces.get(txn.hashCode());
+		// Retrieve the current version
+		DataItem dataItemCopy = dataoperation.getDataItem();
+		log.debug("server" + serverId + "<--[READ(" + dataoperation.getKey() + ")]--coordinator"
+				+ txn.getCoordinatorId());
+		if (pw == null) {
+			pw = new PrivateWorkspace(txn);
+			privateWorkspaces.put(txn.hashCode(), pw);
+			// copy of the dataitem that will be temporary stored in the private workspace
+
+			pw.readCopies.put(dataoperation.getKey(), dataItemCopy);
+		} else {
+			pw.readCopies.put(dataoperation.getKey(), dataItemCopy);
+		}
 		dataoperation.setDataItem(datastore.get(dataoperation.getKey()));
-		// Respond to the coordinator with the serverId, TXN, its data operation and the value in the datastore
+		// Respond to the coordinator with the serverId, TXN, its data operation and the
+		// value in the datastore
 		getSender().tell(new Coordinator.ReadResultMsg(serverId, txn, dataoperation), getSelf());
 	}
-	
+
 	private void OnWriteMsg(Coordinator.WriteMsg msg) {
 		Txn txn = msg.txn;
 		DataOperation dataoperation = msg.dataoperation;
-		
-		
-		//if no private workspace : creation process and write the new data value within
-		
+
+		// if no private workspace : creation process and write the new data value
+		// within
+
 		PrivateWorkspace pw = privateWorkspaces.get(txn.hashCode());
 		// Retrieve the current version
 		Integer value = dataoperation.getDataItem().getValue();
 		DataItem dataItemCopy = dataoperation.getDataItem();
-		
-		// we need to retrieve the version of the data item that is wanted to be overwriten
+
+		// we need to retrieve the version of the data item that is wanted to be
+		// overwriten
 		DataItem dataItemOriginal = datastore.get(msg.dataoperation.getKey());
 		// And increase it
-		dataItemCopy.setVersion(dataItemOriginal.getVersion()+1);
+		dataItemCopy.setVersion(dataItemOriginal.getVersion() + 1);
 		Integer version = dataItemCopy.getVersion();
-		log.debug("server" + serverId + "<--[WRITE(" + dataoperation.getKey() + ")="+value+", version="+version+"]--coordinator" + txn.getCoordinatorId());
-	    if (pw == null) {
-	    	pw = new PrivateWorkspace(txn);
-	    	privateWorkspaces.put(txn.hashCode(), pw);
-	    	//copy of the dataitem that will be temporary stored in the private workspace
-	    	
+		log.debug("server" + serverId + "<--[WRITE(" + dataoperation.getKey() + ")=" + value + ", version=" + version
+				+ "]--coordinator" + txn.getCoordinatorId());
+		if (pw == null) {
+			pw = new PrivateWorkspace(txn);
+			privateWorkspaces.put(txn.hashCode(), pw);
+			// copy of the dataitem that will be temporary stored in the private workspace
+
 			pw.writeCopies.put(dataoperation.getKey(), dataItemCopy);
-	    }
-	    else  {
-	    	pw.writeCopies.put(dataoperation.getKey(), dataItemCopy);
-	    }
+		} else {
+			pw.writeCopies.put(dataoperation.getKey(), dataItemCopy);
+		}
 	}
-	
+
 	private void OnTxnAskVoteMsg(Coordinator.TxnAskVoteMsg msg) {
 		Txn txn = msg.txn;
 		// TODO Lock data items
 		// TODO Validate private workspace
 		Boolean validated = true;
-		
-		getSender().tell(new Server.TxnVoteMsg(txn,validated, serverId), getSelf());
+
+		getSender().tell(new Server.TxnVoteMsg(txn, validated, serverId), getSelf());
 	}
-	
-	
+
 	private void OnTxnVoteResultMsg(Coordinator.TxnVoteResultMsg msg) {
 		Txn txn = msg.txn;
 		boolean commit = msg.commit;
 		PrivateWorkspace pw = privateWorkspaces.get(txn.hashCode());
-		//if the message is commit, we replace the dataItem from the private workspace to the datastore
-		//the msg will be sent to every server so we need to check again if there is a private workspace
-		if (!(pw==null)) {
+		// if the message is commit, we replace the dataItem from the private workspace
+		// to the datastore
+		// the msg will be sent to every server so we need to check again if there is a
+		// private workspace
+		if (!(pw == null)) {
 			if (commit) {
-				for(Integer dataid: pw.writeCopies.keySet()) {
+				for (Integer dataid : pw.writeCopies.keySet()) {
 					datastore.replace(dataid, pw.writeCopies.get(dataid));
-				}	
+				}
 			}
-		
-			// We remove the the private workspace from the server either the decision is commit or not
+
+			// We remove the the private workspace from the server either the decision is
+			// commit or not
 			privateWorkspaces.remove(txn.hashCode());
 			pw = null;
 		}
@@ -147,34 +153,33 @@ public class Server extends AbstractActor {
 
 	@Override
 	public Receive createReceive() {
-		return receiveBuilder()
-				.match(Coordinator.ReadMsg.class, this::OnReadMsg)
+		return receiveBuilder().match(Coordinator.ReadMsg.class, this::OnReadMsg)
 				.match(Coordinator.WriteMsg.class, this::OnWriteMsg)
 				.match(Coordinator.TxnAskVoteMsg.class, this::OnTxnAskVoteMsg)
 				.match(Coordinator.TxnVoteResultMsg.class, this::OnTxnVoteResultMsg).build();
 	}
-	
-    //Depends only on account number
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + serverId;  
-        return result;
-    }
-	
+
+	// Depends only on account number
 	@Override
-    public boolean equals(Object obj) {
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + serverId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
 		if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Server other = (Server) obj;
-        if (serverId != other.serverId)
-            return false;
-        return true;
-    }
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Server other = (Server) obj;
+		if (serverId != other.serverId)
+			return false;
+		return true;
+	}
 
 }
--- src/main/java/it/unitn/ds1/Txn.java
+++ src/main/java/it/unitn/ds1/Txn.java
@@ -3,15 +3,15 @@ package it.unitn.ds1;
 public class Txn {
 	private final Integer coordinatorId;
 	private final Integer clientId;
-	//indicates the state of the transaction :
-	//validated if the TXN is consistent
+	// indicates the state of the transaction :
+	// validated if the TXN is consistent
 	boolean validatedConsistent;
-	//overwriten if all the transient dataItems from the private workspace have been successful
+	// overwriten if all the transient dataItems from the private workspace have
+	// been successful
 	boolean overwritesDone;
-	//number of "commit" votes
+	// number of "commit" votes
 	private Integer votes;
-	
-	
+
 	public Txn(int coordinatorId, int clientId) {
 		this.coordinatorId = coordinatorId;
 		this.clientId = clientId;
@@ -25,7 +25,7 @@ public class Txn {
 	public Integer getClientId() {
 		return clientId;
 	}
-	
+
 	public Integer getVotes() {
 		return votes;
 	}
@@ -33,28 +33,28 @@ public class Txn {
 	public void setVotes(Integer votes) {
 		this.votes = votes;
 	}
-	
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + coordinatorId + clientId;  
-        return result;
-    }
-	
+
 	@Override
-    public boolean equals(Object obj) {
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + coordinatorId + clientId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
 		if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Txn other = (Txn) obj;
-        if (coordinatorId != other.getCoordinatorId())
-            return false;
-        if (clientId != other.getClientId())
-            return false;
-        return true;
-    }
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Txn other = (Txn) obj;
+		if (coordinatorId != other.getCoordinatorId())
+			return false;
+		if (clientId != other.getClientId())
+			return false;
+		return true;
+	}
 }
